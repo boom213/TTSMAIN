216895a0822795109fd967dc4a26defab62ae167
Shubham <shubhamguptaajinkya@gmail.com>
2021-03-15 11:13:47 +0530

    savng new files

---
+++ js/testimonials.js
@@ -0,0 +1,6 @@
+    <link rel="stylesheet" 
+          type="text/css" 
+          href="lightbox2/dist/css/lightbox.min.css"> 
+    <script src= 
+"lightbox2/dist/js/lightbox-plus-jquery.min.js"> 
+    </script> 
